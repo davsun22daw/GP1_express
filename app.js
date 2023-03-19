b691ccc7314ed6bbc11f081a9b66e85034f8c574
MrDeston <davidjoansm@gmail.com>
2023-03-19 21:29:25 +0100

    5: Login y registrar funcional

--- app.js
+++ app.js
@@ -6,7 +6,10 @@ const path = require('path');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
-
+const bcrypt = require('bcrypt');
+const mongoose = require('mongoose');
+const User = require('./models/user');
+const ContrasenyaLogin = 'contraseñaSegura';
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
@@ -28,6 +31,59 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+app.get('/', (req, res) => {
+  res.render('login');
+});
+
+app.get('/crea', (req, res) => {
+  res.render('crea');
+});
+
+app.post('/crea', async (req, res) => {
+  const { nomLogin, ContrasenyaLogin } = req.body;
+  console.log(`User recibida en el backend: ${nomLogin}`);
+  console.log(`Contraseña recibida en el backend: ${ContrasenyaLogin}`);
+
+  try {
+    await mongoose.connect('mongodb://127.0.0.1:27017/GP1');
+    // Crear una nueva instancia del modelo User
+    const user = new User({ nomLogin, ContrasenyaLogin });
+    
+    // Guardar el nuevo usuario en la base de datos
+    await user.save();
+    
+    // Enviar una respuesta al cliente indicando que el usuario ha sido registrado exitosamente
+    res.status(200).send('Usuario registrado exitosamente');
+    
+  } catch (error) {
+    // Manejar errores durante el registro del usuario
+    console.error(`Error al registrar usuario: ${error}`);
+    res.status(500).send('Error al registrar usuario');
+  }
+});
+
+app.post('/comproba', async (req, res) => {
+  const { nomLogin, ContrasenyaLogin } = req.body;
+
+  try {
+    await mongoose.connect('mongodb://127.0.0.1:27017/GP1');
+    const user = await User.findOne({ nomLogin });
+    if (!user) {
+      res.status(404).send('El usuario no existe');
+    } else {
+      const validContrasenyaLogin = await bcrypt.compare(ContrasenyaLogin, user.ContrasenyaLogin);
+      if (validContrasenyaLogin) {
+        // Redirigir al usuario a la página de inicio si la autenticación es exitosa
+        res.redirect('/calendari');
+      } else {
+        res.status(401).send('Contraseña incorrecta');
+      }
+    }
+  } catch (error) {
+    console.error(`Error al autenticar usuario: ${error}`);
+    res.status(500).send('Error al autenticar usuario');
+  }
+});
 
 app.use(function(req, res, next) {
   const error = new Error('Página no encontrada');
@@ -41,7 +97,6 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error', { message: err.message, error: err });
 });
-
 
 httpsServer.listen(443, () => {
   console.log('Es en HTTPS y el protocolo es 443 porsi acaso');
---
+++ models/user.js
@@ -0,0 +1,36 @@
+const bcrypt = require('bcrypt');
+const mongoose = require('mongoose');
+
+const saltRounds = 10;
+
+const UserSchema = new mongoose.Schema({
+  nomLogin: { type: String, required: true, unique: true },
+  ContrasenyaLogin: { type: String, required: true }
+});
+
+UserSchema.pre('save', async function (next) {
+  if (!this.isModified('ContrasenyaLogin')) {
+    return next();
+  }
+
+  try {
+    const salt = await bcrypt.genSalt(saltRounds);
+    const hash = await bcrypt.hash(this.ContrasenyaLogin, salt);
+    this.ContrasenyaLogin = hash;
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+UserSchema.methods.isCorrectContrasenyaLogin = function (ContrasenyaLogin, callback) {
+  bcrypt.compare(ContrasenyaLogin, this.ContrasenyaLogin, function (err, result) {
+    if (err) {
+      callback(err);
+    } else {
+      callback(null, result);
+    }
+  });
+}
+
+module.exports = mongoose.model('User', UserSchema);
